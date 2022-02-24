8d69cd4157c4a9a408d86446640401bac42c0f75
Kotchamon <kotchamon.jk@gmail.com>
2022-02-24 23:19:02 +0700

    upload todoList Prokject 1

---
+++ TodoList/todo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section className='container'>
+      <h1>Todo_List</h1>
+      <form className=''></form>
+
+    </section>
+
+  );
+}
+
+export default App;
