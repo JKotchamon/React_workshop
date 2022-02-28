633fbaf1adafa0c04680a3058498b3e97939de28
Kotchamon <kotchamon.jk@gmail.com>
2022-02-28 23:18:24 +0700

    upload calculator project

---
+++ Calculator/cal-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Calculator from './component/calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator></Calculator>
+ 
+    </div>
+  );
+}
+
+export default App;
